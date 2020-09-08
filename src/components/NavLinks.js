6d4577a9aa588bc5722c32f8651b3d8dae60aa52
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-09-08 11:55:27 -0700

    Add NavLinks component.
    co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: Hannah Beinstein <hannah.beinstein@gmail.com>

---
+++ src/components/NavLinks.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header.js";
+
+function NavLinks() {
+  return (
+    <React.Fragment>
+      <a>Home</a>
+      <a>Notifications</a>
+      <a>Messages</a>
+    </React.Fragment>
+  );
+}
+
+export default NavLinks;
